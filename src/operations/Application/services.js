e52113e81fbc7b2b9428c10326e18b3cfceea019
dhea_chanjaya <Dhea_Chanjaya@Dell.com>
2021-04-22 18:08:44 +0800

    fix ECC-GUI -- pattern way

--- src/operations/Application/services.js
+++ src/operations/Application/services.js
@@ -45,7 +45,39 @@ export async function deployApplication({ id, patternName }) {
     "nodeType": "device", // Only device for now
     "pattern": patternName,
     "arch": "amd64", // DEV
-    "registeredServices": [],
+    "registeredServices": [ // HARDCODE FOR NOW - TODO
+      {
+        "url": "dellsg/com.eos2git.cec.lab.emc.hellosally",
+        "numAgreements": 1,
+        "policy": "{\"header\":{\"name\":\"Policy for dellsg_com.eos2git.cec.lab.emc.hellosally\",\"version\":\"2.0\"},\"apiSpec\":[{\"specRef\":\"com.eos2git.cec.lab.emc.hellosally\",\"organization\":\"dellsg\",\"version\":\"0.0.1\",\"exclusiveAccess\":true,\"arch\":\"amd64\"}],\"valueExchange\":{},\"dataVerification\":{\"metering\":{}},\"proposalRejection\":{},\"properties\":[{\"name\":\"cpus\",\"value\":\"4\"},{\"name\":\"ram\",\"value\":\"7961\"}],\"ha_group\":{},\"nodeHealth\":{}}",
+        "properties": [
+          {
+            "name": "version",
+            "value": "0.0.1",
+            "propType": "version",
+            "op": "in"
+          },
+          {
+            "name": "cpus",
+            "value": "4",
+            "propType": "string",
+            "op": "in"
+          },
+          {
+            "name": "ram",
+            "value": "7961",
+            "propType": "string",
+            "op": "in"
+          },
+          {
+            "name": "arch",
+            "value": "amd64",
+            "propType": "string",
+            "op": "in"
+          }
+        ]
+      }
+    ],
     "userInput": [],
     "msgEndPoint": "",
     "publicKey": "ABCDEF", // DEV
@@ -83,24 +115,74 @@ export async function deployApplication({ id, patternName }) {
     "state": "configured"
   };
 
+  // HARDCODED TODO
+  const userInput = [
+    {
+      "serviceOrgid": "dellsg",
+      "serviceUrl": "com.eos2git.cec.lab.emc.hellosally",
+      "serviceArch": "amd64",
+      "serviceVersionRange": "[0.0.0,INFINITY)",
+      "inputs": [
+        {
+          "name": "KUIPER_IP",
+          "value": "192.168.18.63"
+        },
+        {
+          "name": "KUIPER_PORT",
+          "value": "48075"
+        },
+        {
+          "name": "INFLUXDB_IP",
+          "value": "192.168.1.152"
+        },
+        {
+          "name": "INFLUXDB_PORT",
+          "value": "8086"
+        },
+        {
+          "name": "INFLUXDB_TOKEN",
+          "value": "m8Btf1vDt5vDx6N7f_Yy0RxwvG0wxzvyjslMZ0XxmREtEpZWBsAhUcd5N020NRt2ZltldQlUiIpLD8Km3hcKjQ=="
+        },
+        {
+          "name": "INFLUXDB_CLOUD_TOKEN",
+          "value": "CU4e0jPsdqeSVRL5niynNFJt7pRKG5Xx6ssnPp2Vt4Azd7LFjK0D7Ofg1ElAnfnfW9iJldgLUqDPoFXAbzZ1LA=="
+        },
+        {
+          "name": "BUCKET_NAME",
+          "value": "hello-sally"
+        },
+        {
+          "name": "REDIS_IP",
+          "value": "192.168.18.63"
+        },
+        {
+          "name": "REDIS_PORT",
+          "value": "6379"
+        }
+      ]
+    }
+  ]
+
   // XXX: createNode, initNode and updateNodePolicy should be handle with SDO.
   // XXX: The UI shouldn't require 'manual' registration of the node.
   const createNode = () => fetch(`${HZN_EXCHANGE_API_ENDPOINT}/orgs/${HZN_ORGANIZATION}/nodes/${nodeDefinition.name}`, { ...opts, method: 'PUT', body: JSON.stringify(nodeDefinition) });
   const initNode = () => fetch(HZN_AGENT_NODE_API_ENDPOINT, { ...opts, method: 'POST', body: JSON.stringify(nodeConfig) });
   const updateNodePolicy = () => fetch(`${HZN_AGENT_NODE_API_ENDPOINT}/policy`, { ...opts, method: 'POST', body: JSON.stringify(nodePolicy) });
   const registerNode = () => fetch(`${HZN_AGENT_NODE_API_ENDPOINT}/configstate`, { ...opts, method: 'PUT', body: JSON.stringify(nodeState) });
+  const updateNodeUserInput = () => fetch(`${HZN_AGENT_NODE_API_ENDPOINT}/userinput`, { ...opts, method: 'POST', body: JSON.stringify(userInput) });
 
   await createNode();
   await initNode();
   await updateNodePolicy();
-  return await registerNode();
+  await registerNode();
+  return await updateNodeUserInput();
 }
 
 // As of now, there is no need to pass any id.
 // There is only 1 agent running registered with 1 exchange.
 // This is a limitation from OH itself
 export function uninstall() {
-  return fetch(`${HZN_AGENT_NODE_API_ENDPOINT}?block=true&removeNode=true`, { ...options, method: 'DELETE' })
+  return fetch(`${HZN_AGENT_NODE_API_ENDPOINT}?block=true&deepClean=true`, { ...options, method: 'DELETE' })
     .then(response => {
       if (response.status !== 204) {
         throw Error(response.statusText);
